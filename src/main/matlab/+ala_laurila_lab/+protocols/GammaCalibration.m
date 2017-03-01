559baf99a55be79ccdf3a14c20e66613ec8e6974
Sathish Narayanan <ragav.sathish@gmail.com>
2017-03-02 00:43:47 +0200

    preparing for gamma calibration

---
+++ src/main/matlab/+ala_laurila_lab/+protocols/GammaCalibration.m
@@ -0,0 +1,58 @@
+classdef GammaCalibration < sa_labs.protocols.StageProtocol
+    
+    properties
+        
+        preTime = 500           % Spot leading duration (ms)
+        stimTime = 1000         % Spot duration (ms)
+        tailTime = 1000         % Spot trailing duration (ms)
+        intensity = 1           % spot intensity 
+        spotSize = 200;         % Spot size in (um)
+        numberOfEpochs = 100;   % number of epochs
+    end
+    
+    
+    properties (Hidden, Dependent)
+        totalNumEpochs
+    end
+    
+    methods
+        
+        function p = createPresentation(obj)
+            p = stage.core.Presentation((obj.preTime + obj.stimTime + obj.tailTime) * 1e-3);
+            
+            spot = stage.builtin.stimuli.Ellipse();
+            spot.radiusX = round(obj.um2pix(obj.spotSize / 2));
+            spot.radiusY = spot.radiusX;
+            spot.color = obj.intensity;
+            spot.opacity = 1;
+            canvasSize = obj.rig.getDevice('Stage').getCanvasSize();
+            spot.position = canvasSize / 2;
+            p.addStimulus(spot);
+            
+            function c = onDuringStim(state, preTime, stimTime)
+                c = 1 * (state.time>preTime*1e-3 && state.time<=(preTime+stimTime)*1e-3);
+            end
+            
+            function c = patternSelect(state, activePatternNumber)
+                c = 1 * (state.pattern == activePatternNumber - 1);
+            end
+            
+            if obj.numberOfPatterns > 1
+                pattern = obj.primaryObjectPattern;
+                controller = stage.builtin.controllers.PropertyController(spot, 'color', ...
+                    @(s)(onDuringStim(s, obj.preTime, obj.stimTime) || patternSelect(s, pattern)));
+            else
+                controller = stage.builtin.controllers.PropertyController(spot, 'opacity', ...
+                    @(s)onDuringStim(s, obj.preTime, obj.stimTime));
+            end
+            p.addController(controller);
+            
+        end
+        
+        function totalNumEpochs = get.totalNumEpochs(obj)
+            totalNumEpochs = obj.numberOfEpochs;
+        end
+        
+    end
+    
+end
---
+++ src/main/matlab/+ala_laurila_lab/+rigs/AaltoPatchRigCailbration.m
@@ -0,0 +1,22 @@
+classdef AaltoPatchRigCailbration < ala_laurila_lab.AaltoPatchRig
+    
+    methods
+        
+        function obj = AaltoPatchRigCailbration()
+            import symphonyui.builtin.daqs.*;
+            import symphonyui.builtin.devices.*;
+            import symphonyui.core.*;
+            
+            daq = HekaDaqController(HekaDeviceType.ITC1600);
+            obj.daqController = daq;
+            
+            lightCrafter = fi.helsinki.biosci.ala_laurila.devices.LightCrafterDevice('micronsPerPixel', obj.micronsPerPixel);
+            lightCrafter.setConfigurationSetting('frameTrackerPosition', obj.frameTrackerPosition)
+            lightCrafter.setConfigurationSetting('frameTrackerSize', obj.frameTrackerSize)
+            obj.addDevice(lightCrafter);
+        end
+        
+    end
+    
+end
+
