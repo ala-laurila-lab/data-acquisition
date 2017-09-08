a4a394934aa43fb50587d1ed76f7f52ea907138e
Sathish Narayanan <ragav.sathish@gmail.com>
2017-09-08 12:15:43 +0300

    Add alignment cross

---
+++ src/main/matlab/+ala_laurila_lab/+protocols/AlignmentCross.m
@@ -0,0 +1,8 @@
+classdef AlignmentCross < sa_labs.protocols.stage.AlignmentCross
+
+    % It will just have the documentation of the protocol.
+    % 
+    % The actual protocol lives in lib/sa-labs-extension/src/main/matlab/+sa_labs/+protocols/+stage
+    
+end
+
