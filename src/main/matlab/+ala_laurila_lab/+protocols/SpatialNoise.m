d66b28238d81b84a37c8c9135f3ebfaa0b180cf3
Sathish Narayanan <ragav.sathish@gmail.com>
2017-09-08 12:19:27 +0300

    Add LightStep, SpatialNoise

---
+++ src/main/matlab/+ala_laurila_lab/+protocols/SpatialNoise.m
@@ -0,0 +1,8 @@
+classdef SpatialNoise < sa_labs.protocols.stage.SpatialNoise
+
+    % It will just have the documentation of the protocol.
+    % 
+    % The actual protocol lives in lib/sa-labs-extension/src/main/matlab/+sa_labs/+protocols/+stage
+    
+end
+
