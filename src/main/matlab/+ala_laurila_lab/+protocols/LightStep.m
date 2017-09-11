9e2088e43bbb6efac886236eff0ddc314635c867
Sathish Narayanan <ragav.sathish@gmail.com>
2017-09-11 15:26:37 +0300

    WIP for white noise flicker

--- src/main/matlab/+ala_laurila_lab/+protocols/LightStep.m
+++ src/main/matlab/+ala_laurila_lab/+protocols/LightStep.m
@@ -1,8 +1,6 @@
 classdef LightStep < sa_labs.protocols.stage.LightStep
 
     % This file contains the protocol documentation
-    % 
-    %
     % The actual protocol lives in lib/sa-labs-extension/src/main/matlab/+sa_labs/+protocols/+stage
     
 end
