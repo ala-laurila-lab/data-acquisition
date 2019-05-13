383874fd55f1f933233cf65fe011ada98e54f40c
JohanWesto <johan.westo@gmail.com>
2019-05-13 10:32:43 +0300

    update protocol to match 600 um increment decrement in Viikki

--- src/main/matlab/+ala_laurila_lab/+protocols/DecrementIncrement.m
+++ src/main/matlab/+ala_laurila_lab/+protocols/DecrementIncrement.m
@@ -8,7 +8,7 @@ classdef DecrementIncrement < sa_labs.protocols.StageProtocol & sa_labs.common.P
         
         spotSize = 170; % um
         numberOfEpochs = 30;
-        decrementOnly = false;
+        decrement = true;
         randomOrdering = true;         % ramdom presentation order
     end
     
@@ -33,7 +33,7 @@ classdef DecrementIncrement < sa_labs.protocols.StageProtocol & sa_labs.common.P
             obj.logPrepareRun();
             prepareRun@sa_labs.protocols.StageProtocol(obj);
             
-            obj.numberOfCombinations = 1 + ~obj.decrementOnly;
+            obj.numberOfCombinations = 1;
             obj.order = 1:obj.numberOfCombinations;
             
         end
@@ -48,7 +48,7 @@ classdef DecrementIncrement < sa_labs.protocols.StageProtocol & sa_labs.common.P
             end
             
             obj.epochIdx = obj.order(index);
-            if obj.epochIdx == 1
+            if obj.decrement
                 obj.intensity = 0;
             else
                 obj.intensity = 2*obj.meanLevel;
@@ -65,16 +65,32 @@ classdef DecrementIncrement < sa_labs.protocols.StageProtocol & sa_labs.common.P
         function p = createPresentation(obj)
             p = stage.core.Presentation((obj.preTime + obj.stimTime + obj.tailTime) * 1e-3);
             
+            canvasSize = obj.rig.getDevice('Stage').getCanvasSize();
+            
             spot = stage.builtin.stimuli.Ellipse();
             spot.radiusX = round(obj.um2pix(obj.spotSize / 2));
             spot.radiusY = spot.radiusX;
             spot.color = obj.intensity;
-            canvasSize = obj.rig.getDevice('Stage').getCanvasSize();
             spot.position = canvasSize / 2;
             p.addStimulus(spot);
             
-            obj.setOnDuringStimController(p, spot);
+            obj.changeStimIntensityController(p, spot);
             
+        end
+        
+        function changeStimIntensityController(obj, p, stageObject)
+            function c = changeDuringStim(state, preTime, stimTime, intensity, meanLevel)
+                stim = (state.time>preTime*1e-3 && state.time<=(preTime+stimTime)*1e-3);
+                if stim
+                    c = intensity;
+                else
+                    c = meanLevel;
+                end
+            end
+            
+            controller = stage.builtin.controllers.PropertyController(stageObject, 'color', ...
+                @(s)changeDuringStim(s, obj.preTime, obj.stimTime, obj.intensity, obj.meanLevel));
+            p.addController(controller);
         end
         
         
